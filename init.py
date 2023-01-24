3c11b92b6ccb2716456bf4fafc3ba54c579f179f
king <jasonpop300@gmail.com>
2023-01-23 19:39:24 -0600

    Fixed init.py

--- init.py
+++ init.py
@@ -2,4 +2,4 @@ import subprocess
 import os
 
 # Install the dependencies from the requirements.txt file
-subprocess.run(["pip", "install", "-r", os.path.abspath("requirements.txt")], cwd='./venv')
+subprocess.run(["pip", "install", "-r", os.path.abspath("requirements.txt")])
