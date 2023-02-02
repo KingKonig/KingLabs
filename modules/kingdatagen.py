f221023e8408e88c0e5e70986d03e17f484f27e9
king <jasonpop300@gmail.com>
2023-02-02 17:02:33 -0600

    Data gen started

--- modules/kingdatagen.py
+++ modules/kingdatagen.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import numpy as np
 
 
 def express_gen(expression, upper_lim, lower_lim):
-    df = pd.DataFrame
+    df = pd.DataFrame(columns=["x", "y"])
     for x in range(int(lower_lim), int(upper_lim)):
         y = eval(expression)
-        print(f"({x}, {y})")
+        # print(f"({x}, {y})")
+        # df. concat to df
+
 
 
 if __name__ == "__main__":
